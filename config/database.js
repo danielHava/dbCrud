574f10899e40968b5532138edbca833730208fb0
Daniel Hava <dani.hava@protonmail.com>
2019-06-28 16:53:03 +0300

    fixed not setting vars from .env

--- config/database.js
+++ config/database.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASS,
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
-    dialect: 'postgresql'
+    dialect: 'postgresql',
+  },
+  test: {
+    username: 'user_test',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgresql',
+  },
+  production: {
+    username: 'DB_USERNAME',
+    password: 'DB_PASSWORD',
+    database: 'DB_NAME',
+    host: 'DB_HOSTNAME',
+    dialect: 'postgresql',
   }
 };
